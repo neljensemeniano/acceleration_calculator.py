f437ee86875195f1828e09d1667094a1f04fea43
neljensemeniano <125987223+neljensemeniano@users.noreply.github.com>
2023-07-26 11:14:38 +0300

    Update acceleration_calculator.py

--- acceleration_calculator.py
+++ acceleration_calculator.py
@@ -1,5 +1,7 @@
 force = float(input("Введите силу (в ньютонах): "))
 mass = float(input("Введите массу тела (в килограммах): "))
 
+
+
 acceleration = force / mass
 print("Ускорение тела равно", acceleration, "м/с².")
